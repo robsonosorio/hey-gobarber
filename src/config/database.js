9f6b7667de71491b4aa9398b2e11a3e5e95c7253
Robson Osorio <robsonosorio@GX2-135.gx2.local>
2020-09-16 13:59:16 -0300

    add e configurado sequelize

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'hey-gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
+
+UserGroup
+
+UserGroups
+
+user_groups
