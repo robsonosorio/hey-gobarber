b95b8af2cca2e055afdaa6e8b06347d2bd27c4e4
Robson Osorio <robson.osorioo@gmail.com>
2020-09-17 15:44:01 -0300

    update de usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -14,9 +14,25 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword} = req.body;
 
-    return res.json({ ok: true });
+    const user = await User.findByPk(req.userId);
+
+    if (email != user.emial) {
+      const userExists = await User.findOne({ where: { email: email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Email já registrado.'});
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Senha inválida.' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
